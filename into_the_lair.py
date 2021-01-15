5007ff8383fb1243efe97165047ea24af9894f0a
Gerard Gandionco <70365928+Vaiterius@users.noreply.github.com>
2021-01-14 22:27:05 -0800

    Add files via upload

---
+++ into_the_lair.py
@@ -0,0 +1,19 @@
+"""
+Created on Fri Nov 6 2020
+
+@author: Gerard G.
+
+"Into The Lair"
+
+Made to feel like a demo for a much larger RPG. Features rooms
+to navigate to and from, an inventory with equipable/usable
+items, combat encounters with enemies, and dialogue with NPCs,
+leveling system, and... easter eggs?
+"""
+
+import sys
+from engine.main import main
+
+if __name__ == "__main__":
+    main()
+    sys.exit()
